2e1abfa7ed49c416e649fea6435d25cf091569e0
Tore Dahl <tore.dahl@bouvet.no>
2014-02-16 14:34:18 +0100

    new server

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-connect = require('connect');
+varr gzippo = require('gzippo');
+var express = require('express');
+var app = express();
 var port = 5000;
-connect.createServer(
-    connect.static(__dirname)
-).listen(process.env.PORT || port);
+process.env.PWD = process.cwd();
 
+app.configure(function() {
+    app.use(express.static(process.env.PWD + '/'));
+});
+app.get('*', function(req, res) {
+     res.sendfile('./app/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + process.env.PWD + "/dist"));
+app.listen(process.env.PORT || port);
