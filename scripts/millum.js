2c0d778044649f878dbe47125a06849ed559820c
Tore Dahl <tore.dahl@bouvet.no>
2014-03-05 15:18:21 +0100

    further adjustments for mobile viewing

--- scripts/millum.js
+++ scripts/millum.js
@@ -9,12 +9,6 @@
 (function(a){a.tiny=a.tiny||{};a.tiny.carousel={options:{start:1,display:1,axis:"x",controls:true,pager:false,interval:false,intervaltime:3000,rewind:false,animation:true,duration:1000,callback:null}};a.fn.tinycarousel_start=function(){a(this).data("tcl").start()};a.fn.tinycarousel_stop=function(){a(this).data("tcl").stop()};a.fn.tinycarousel_move=function(c){a(this).data("tcl").move(c-1,true)};function b(q,e){var i=this,h=a(".viewport:first",q),g=a(".overview:first",q),k=g.children(),f=a(".next:first",q),d=a(".prev:first",q),l=a(".pager:first",q),w=0,u=0,p=0,j=undefined,o=false,n=true,s=e.axis==="x";function m(){if(e.controls){d.toggleClass("disable",p<=0);f.toggleClass("disable",!(p+1<u))}if(e.pager){var x=a(".pagenum",l);x.removeClass("active");a(x[p]).addClass("active")}}function v(x){if(a(this).hasClass("pagenum")){i.move(parseInt(this.rel,10),true)}return false}function t(){if(e.interval&&!o){clearTimeout(j);j=setTimeout(function(){p=p+1===u?-1:p;n=p+1===u?false:p===0?true:n;i.move(n?1:-1)},e.intervaltime)}}function r(){if(e.controls&&d.length>0&&f.length>0){d.click(function(){i.move(-1);return false});f.click(function(){i.move(1);return false})}if(e.interval){q.hover(i.stop,i.start)}if(e.pager&&l.length>0){a("a",l).click(v)}}this.stop=function(){clearTimeout(j);o=true};this.start=function(){o=false;t()};this.move=function(y,z){p=z?y:p+=y;if(p>-1&&p<u){var x={};x[s?"left":"top"]=-(p*(w*e.display));g.animate(x,{queue:false,duration:e.animation?e.duration:0,complete:function(){if(typeof e.callback==="function"){e.callback.call(this,k[p],p)}}});m();t()}};function c(){w=s?a(k[0]).outerWidth(true):a(k[0]).outerHeight(true);var x=Math.ceil(((s?h.outerWidth():h.outerHeight())/(w*e.display))-1);u=Math.max(1,Math.ceil(k.length/e.display)-x);p=Math.min(u,Math.max(1,e.start))-2;g.css(s?"width":"height",(w*k.length));i.move(1);r();return i}return c()}a.fn.tinycarousel=function(d){var c=a.extend({},a.tiny.carousel.options,d);this.each(function(){a(this).data("tcl",new b(a(this),c))});return this}}(jQuery));
 
 
-$("div.second-row-inner-container").on("swipe", function(){
-      alert("you swiped it");
-
-      event.preventDefault();
-  });
-
 //= responsive design, built with foundation: wordpress use required+ foundation:  http://themes.required.ch/  Recommend using a child theme of this theme for wordpress implementation.
 // documentation for foundation can be found here: http://foundation.zurb.com/docs/
 
@@ -70,6 +64,14 @@ function startTinySlideshows(startslider) {
 //.tinycarousel({interval: true,  axis: 'y', intervaltime: 2000, duration: 1000, pager:true});      
 // we use Ajax complete, because we are loading the footer and header element on every page. Redo this in wordpress as desired. 
 $( document ).ajaxComplete(function() {
+
+
+  // $("div.second-row-inner-container").on("tap", function(){
+  //     $("div.second-row-inner-container img").hide();
+  //     event.preventDefault();
+  // });
+
+
   var frontPage = $(".overlay-container").length > 0;
   if ( frontPage) {
     createOverlay();
@@ -146,7 +148,7 @@ $( document ).ajaxComplete(function() {
   var has_focus = true;
 
   if(has_focus) {
-    startTinySlideshows(true);
+    //startTinySlideshows(true);
   }
 
   // the idea here was to halt the slideshows if nobody was watching, i.e window.onblur, but have not had the time to look into that. Leave it as a Nice to have/TODO option. 
